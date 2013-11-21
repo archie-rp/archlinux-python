876f798394ba0ac84e4c2399f1b21985ba71b41c
archie-rp <archlinuxrp@gmail.com>
2013-11-21 02:01:44 +0000

    fix

--- test.py
+++ test.py
@@ -87,7 +87,7 @@ def instalar(x):
 	print (colored("Instalar pacotes: ", 'cyan', attrs=['bold']))
 	print (linha)
 	print (colored("A installar :", 'green') + '"' + colored(x, 'blue') + '"')
-	os.system('yaourt -Syyu --noconfirm ' + x)
+	os.system('pacman -Syyu --noconfirm ' + x)
 	input("regista apps")
 	pass
 
@@ -211,22 +211,21 @@ while sair != "x":
  			xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbutils \
  			xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmessage \
 			xorg-xmodmap xorg-xpr xorg-xprop xorg-xsetroot xorg-xvinfo \
-			xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xwd '
-            #pkgtoinstall2 = "xorg-xwininfo xorg-xwud ttf-dejavu xterm zsh \
-            #pcmanfm thunar lxappearance mirage file-roller udisks udisks2 \
-            #polkit polkit-gnome gvfs gvfs-smb bash-completion udiskie chromium \
-            #zip unrar tar autofs ntfs-3g thunar-archive-plugin thunar-volman pidgin \
-            #skype curl git wget mplayer vlc ttf-liberation ttf-freefont lxappearance \
- 			#bc rsync mlocate bash-completion pkgstats ntfs-3g dosfstools exfat-utils fuse \
- 			#fuse-exfat openssh nfs-utils samba smbnetfs  gamin rxvt-unicode pcmanfm gvfs scrot \
- 			#thunar tumbler "
- 			#pkgtoinstall3='leafpad epdfview nitrogen ttf-bitstream-vera ttf-dejavu \
- 			#wicd wicd-gtk libmtp gvfs-mtp jdk7-openjdk icedtea-web-java7 htop \
- 			#chromium firefox transmission-gtk pidgin skype gst-plugins-base \
- 			#gst-plugins-base-libs gst-plugins-good gst-plugins-bad \
- 			#gst-plugins-ugly gst-libav vlc xbmc libbluray libquicktime \
- 			#weechat imap faenza-icon-theme ttf-dejavu tamsyn-font \
- 			#libdvdread libdvdnav libdvdcss'
+			xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xwd \
+			xorg-xwininfo xorg-xwud ttf-dejavu xterm zsh \
+            pcmanfm thunar lxappearance mirage file-roller udisks udisks2 \
+            polkit polkit-gnome gvfs gvfs-smb bash-completion udiskie chromium \
+            zip unrar tar autofs ntfs-3g thunar-archive-plugin thunar-volman pidgin \
+            skype curl git wget mplayer vlc ttf-liberation ttf-freefont lxappearance \
+ 			bc rsync mlocate bash-completion pkgstats ntfs-3g dosfstools exfat-utils fuse \
+ 			fuse-exfat openssh nfs-utils samba smbnetfs  gamin rxvt-unicode pcmanfm gvfs scrot \
+ 			thunar tumbler leafpad epdfview nitrogen ttf-bitstream-vera ttf-dejavu \
+ 			wicd wicd-gtk libmtp gvfs-mtp jdk7-openjdk icedtea-web-java7 htop \
+ 			chromium firefox transmission-gtk pidgin skype gst-plugins-base \
+ 			gst-plugins-base-libs gst-plugins-good gst-plugins-bad \
+ 			gst-plugins-ugly gst-libav vlc xbmc libbluray libquicktime \
+ 			weechat imap faenza-icon-theme ttf-dejavu tamsyn-font \
+ 			libdvdread libdvdnav libdvdcss'
 			extra = ('mediterraneannight-theme compton-git adwaita-x-dark-and-light-theme gtk-theme-hope zukitwo-themes \
 				gtk-theme-elementary mate-icon-theme-faenza weeplugins-git nano-syntax-highlighting-git \
 				gnome-theme-adwaita zsh-syntax-highlighting android-sdk android-apktool android-sdk-build-tools \
